2a26b697a4b16e2fabccf5e747426ca2d88dcf40
Miksyu <vozmilova05@gmail.com>
2021-05-11 11:51:09 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ const log = document.querySelector('.login');
 const popup = document.querySelector('.contacts-link');
 const close = document.querySelector('.modal-close');
 const close1 = document.querySelector('.close-1');
+const guaranteeBut = document.querySelector('.services-bottom-item-guarantee');
+const deliveryBut = document.querySelector('.services-bottom-item-delivery')
+const guarantee = document.querySelector('.services-guarantee');
+const delivery = document.querySelector('.services-delivery');
+const credit = document.querySelector('.services-credit');
+
 
 popup.addEventListener("click", function(){
   feedback.classList.remove('visually-hidden');
@@ -22,3 +28,22 @@ close1.addEventListener("click", function(){
   login.classList.add("visually-hidden")
 })
 
+
+
+const getClickGuarantee = guaranteeBut.onclick = function (){
+  guarantee.classList.remove('visually-hidden')
+  delivery.classList.add("visually-hidden")
+  credit.classList.add("visually-hidden")
+  deliveryBut.classList.remove("white-color")
+  guaranteeBut.classList.add("white-color")
+}
+
+/*const getClickDelivery = guaranteeBut.onclick = function (){
+  delivery.classList.remove('visually-hidden')
+  credit.classList.add("visually-hidden")
+  guarantee.classList.add("visually-hidden")
+}
+*/
+byuInCart.addEventListener("click", function(){
+  catalog.classList.remove('visually-hidden');
+})
